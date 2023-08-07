2d8ab74ec9fd9f49e5a174eec611cce2ae0f4b83
moshe peretz <moshedota@gmail.com>
2023-08-07 18:18:39 +0300

    ep34 search bar and clean up code

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>DarkMode</Text>
+      <Text whiteSpace="nowrap">DarkMode</Text>
     </HStack>
   );
 };
